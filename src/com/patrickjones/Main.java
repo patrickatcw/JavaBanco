ee6bbc16a303a61d506fb9c2fa4882867375d5f0
patrickatcw <pj8392@yahoo.com>
2018-02-02 10:16:19 -0500

    created ryan's account in main

--- src/com/patrickjones/Main.java
+++ src/com/patrickjones/Main.java
@@ -1,8 +1,22 @@
 package com.patrickjones;
 
+import com.patrickjones.models.BancAccount;
+
 public class Main {
 
     public static void main(String[] args) {        //psvm then tab
+
+        BancAccount patrick = new BancAccount();        //new object, creating an instance of the class BancAccount
+        patrick.setAccountName("Patrick's Account");
+        patrick.setBalance(1_000);
+        int patrickAccountId = 1;
+        patrick.setAccountNumber(patrickAccountId);
+
+        BancAccount ryan = new BancAccount();
+        ryan.setAccountName("Ryan's Account");
+        ryan.setBalance(10_000);
+        int ryanAccountId = 2;
+        ryan.setAccountNumber(ryanAccountId);
 
     }
 }
