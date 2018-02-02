5062d86fc3a60b470d70f94dcbf6a78f12d9c083
patrickatcw <pj8392@yahoo.com>
2018-02-02 09:43:12 -0500

    added setters and getters to class BancAccount

--- src/com/patrickjones/models/BancAccount.java
+++ src/com/patrickjones/models/BancAccount.java
@@ -6,4 +6,27 @@ public class BancAccount {
     private int balance;
     private int accountNumber;
 
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 }
