6dd354cc665c2574c6ae0bd895c839462530b2a3
patrickatcw <pj8392@yahoo.com>
2018-02-08 10:07:59 -0500

    current status on assignment

--- src/com/patrickjones/Main.java
+++ src/com/patrickjones/Main.java
@@ -1,12 +1,15 @@
 package com.patrickjones;
 
 import com.patrickjones.models.BancAccount;
+import com.patrickjones.services.TransferManager;
+
+
 
 public class Main {
 
     public static void main(String[] args) {        //psvm then tab
 
-        BancAccount patrick = new BancAccount();        //new object, creating an instance of the class BancAccount
+        BancAccount patrick = new BancAccount();    //new object, creating an instance of the class BancAccount
         patrick.setAccountName("Patrick's Account");
         patrick.setBalance(1_000);
         int patrickAccountId = 1;
@@ -18,12 +21,26 @@ public class Main {
         int ryanAccountId = 2;
         ryan.setAccountNumber(ryanAccountId);
 
+        //use the transfer manager to transfer $50 from patrick to ryan
+
+        // steps:
+
+        // 1. Create a new instance of the TransferManager class
+
+        // 2. call the `transfer` method of the instance you created in step 1,
+        // passing in your variables patricksAccount, ryansAccount
+        // and the amount you want to transfer
+
         System.out.println(patrick);
         System.out.println(ryan);
 
-    //use the transfer manager to transfer $50 from patrick to ryan
-        System.out.println(patrick);
-        System.out.println(ryan);
+
+        TransferManager myTransferManager = new TransferManager( );//this creates a new object myTransferManager
+            myTransferManager.transfer(60);
+            myTransferManager.getTransfer();
+            System.out.println();
 
     }
 }
+
+
--- src/com/patrickjones/services/TransferManager.java
+++ src/com/patrickjones/services/TransferManager.java
@@ -1,12 +1,60 @@
 package com.patrickjones.services;
 
-import com.patrickjones.models.BancAccount;
+//object - The principal building blocks of object-oriented programs. Each object is a programming
+// unit consisting of data (instance variables) and functionality (instance methods).
+//instance - An object of a particular class. In programs written in the Java(TM) programming language,
+// an instance of a class is created using the new operator followed by the class name.
+//instance variable - Any item of data that is associated with a particular object.
+// Each instance of a class has its own copy of the instance variables defined in the class.
+// Also called a field. See also class variable.
+//instance method - Any method that is invoked with respect to an instance of a class.
+// Also called simply a method. See also class method.
 
-public class TransferManager {
+public class TransferManager {                                                     //class
 
-    public void transfer(BancAccount accounTtoDebit, BancAccount accountToCredit, int ammountToTransfer){
+    int process;
+    //instance, instance variable, or object, which could be a characteristic or action
 
-        //homework to complete this method, the logic to transfer money, addition / subtraction.
+    public void transfer(/*BancAccount accountToDebit, BancAccount accountToCredit,*/
+            int amountToTransfer) {     //constructor or instance method with 3 parameters
+        process = amountToTransfer;
+        }
 
-    }
+        public int getTransfer(){
+            System.out.println("The amount to transfer is " + process);
+        return process;
+        }
+
 }
+
+// steps:
+
+// 1. Create a new instance of the TransferManager class (instance and object may be the same?)
+
+// 2. call the `transfer` method of the instance you created in step 1,
+// passing in your variables patricksAccount, ryansAccount
+// and the amount you want to transfer
+
+// 1. get the current balance of the accountToDebit
+
+// 2. subtract the amountToTransfer from the current balance of accountToDebit
+
+// 3. update the balance of the accountToDebit with the new balance you calculated
+// in step 2 using the setBalance method
+
+// 4. get the current balance of the accountToCredit
+
+// 5. increase the balance of accountToCredit by amountToTransfer
+
+// 6. set the updated balance of accountToCredit to the value you calculated in
+// step 5 by using the setBalance method
+
+//homework to complete this method, the logic to transfer money, addition / subtraction.
+//use the transfer manager to transfer $60 from patrick to ryan
+
+/*what do you want to do?
+create a method that transfers money from one account to another
+what are the problems?
+- create variables and method logic
+- the parameters; how to identify, the int is already there for one, but the others?
+*/
