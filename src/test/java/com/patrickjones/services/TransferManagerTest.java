d247658be20b75c6138c54da96df0e0c5b8b6cd1
patrickatcw <pj8392@yahoo.com>
2018-02-19 14:56:15 -0500

    fixed testing error, commented results at bottom of page

--- src/test/java/com/patrickjones/services/TransferManagerTest.java
+++ src/test/java/com/patrickjones/services/TransferManagerTest.java
@@ -1,11 +1,9 @@
 package com.patrickjones.services;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Assert;
+import com.patrickjones.models.BancAccount;
 import org.junit.Test;
 
-import com.patrickjones.models.BancAccount;
+import static org.junit.Assert.assertEquals;
 
 public class TransferManagerTest {
 
@@ -26,7 +24,45 @@ public class TransferManagerTest {
 
 	@Test
 	public void testCreditsAccountToCreditByTransferAmount() {
-		Assert.fail("Patrick has to complete me");
+        //Assert.fail("Patrick has to complete me");   test error to complete
+
+        BancAccount acctToDebit = new BancAccount();
+        acctToDebit.setBalance(1000);
+
+        BancAccount acctToCredit = new BancAccount();
+        acctToCredit.setBalance(700);
+
+        TransferManager transferManager = new TransferManager();
+        transferManager.transfer(acctToDebit, acctToCredit, 100);
+
+        String failureMessage = "The account to credit's balance should be increased by the amount to transfer";
+        assertEquals(failureMessage, 800, acctToCredit.getBalance());
+
 	}
 
 }
+
+/* Test Results
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Running com.patrickjones.services.TransferManagerTest
+1000
+700
+1000
+700
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.093 sec
+
+Results :
+
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0
+
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD SUCCESS
+[INFO] ------------------------------------------------------------------------
+[INFO] Total time: 2.799 s
+[INFO] Finished at: 2018-02-19T14:53:06-05:00
+[INFO] Final Memory: 13M/43M
+[INFO] ------------------------------------------------------------------------
+
+*/
