8dec2c19b9238b2397ead6db01724859151275f9
patrickatcw <pj8392@yahoo.com>
2018-02-22 17:54:14 -0500

    fixed testing error, commented results at bottom of page

--- src/test/java/com/patrickjones/services/TransferManagerTest.java
+++ src/test/java/com/patrickjones/services/TransferManagerTest.java
@@ -7,23 +7,23 @@ import static org.junit.Assert.assertEquals;
 
 public class TransferManagerTest {
 
-	@Test
-	public void testDebitsAccountToDebitByTransferAmount() {
-		BancAccount acctToDebit = new BancAccount();
-		acctToDebit.setBalance(1000);
+    @Test
+    public void testDebitsAccountToDebitByTransferAmount() {
+        BancAccount acctToDebit = new BancAccount();
+        acctToDebit.setBalance(1000);
 
-		BancAccount acctToCredit = new BancAccount();
-		acctToCredit.setBalance(700);
+        BancAccount acctToCredit = new BancAccount();
+        acctToCredit.setBalance(700);
 
-		TransferManager transferManager = new TransferManager();
-		transferManager.transfer(acctToDebit, acctToCredit, 100);
+        TransferManager transferManager = new TransferManager();
+        transferManager.transfer(acctToDebit, acctToCredit, 100);
 
-		String failureMessage = "The account to debit's balance should be reduced by the amount to transfer";
-		assertEquals(failureMessage, 900, acctToDebit.getBalance());
-	}
+        String failureMessage = "The account to debit's balance should be reduced by the amount to transfer";
+        assertEquals(failureMessage, 900, acctToDebit.getBalance());
+    }
 
-	@Test
-	public void testCreditsAccountToCreditByTransferAmount() {
+    @Test
+    public void testCreditsAccountToCreditByTransferAmount() {
         //Assert.fail("Patrick has to complete me");   test error to complete
 
         BancAccount acctToDebit = new BancAccount();
@@ -38,31 +38,27 @@ public class TransferManagerTest {
         String failureMessage = "The account to credit's balance should be increased by the amount to transfer";
         assertEquals(failureMessage, 800, acctToCredit.getBalance());
 
-	}
+    }
+
+    @Test
+    public void testAccountToDebitBalance() {
+
+        BancAccount acctToDebitBalance = new BancAccount();
+        acctToDebitBalance.setBalance(1000);
+
+        BancAccount acctToCreditBalance = new BancAccount();
+        acctToCreditBalance.setBalance(700);
+
+        BancAccount amountToTransfer = new BancAccount();
+        amountToTransfer.setBalance(1001);
+
+        TransferManager transferManager = new TransferManager();
+        transferManager.transfer(acctToDebitBalance, acctToCreditBalance, 1001);
+
+        String failureMessage = "The account to debit's balance does not have enough funds to complete this transfer";
+        assertEquals(failureMessage, -1, amountToTransfer.getBalance());
+
+    }
 
 }
 
-/* Test Results
--------------------------------------------------------
- T E S T S
--------------------------------------------------------
-Running com.patrickjones.services.TransferManagerTest
-1000
-700
-1000
-700
-Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.093 sec
-
-Results :
-
-Tests run: 2, Failures: 0, Errors: 0, Skipped: 0
-
-[INFO] ------------------------------------------------------------------------
-[INFO] BUILD SUCCESS
-[INFO] ------------------------------------------------------------------------
-[INFO] Total time: 2.799 s
-[INFO] Finished at: 2018-02-19T14:53:06-05:00
-[INFO] Final Memory: 13M/43M
-[INFO] ------------------------------------------------------------------------
-
-*/
