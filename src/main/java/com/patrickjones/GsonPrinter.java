f8064d3def96f3577e74b1b5db52e677c99c40a7
patrickatcw <pj8392@yahoo.com>
2018-03-16 19:59:07 -0400

    working on method List<BancAccount> createBanAccount, cannot return 2 values

--- src/main/java/com/patrickjones/GsonPrinter.java
+++ src/main/java/com/patrickjones/GsonPrinter.java
@@ -6,18 +6,26 @@ package com.patrickjones;
 import com.google.gson.Gson;
 import com.patrickjones.models.BancAccount;
 
+import java.util.Collections;
+import java.util.List;
+
 public class GsonPrinter {
 
     public static void main(String[] args) {
 
-        BancAccount ryan = createBankAccount() ;
+        List<BancAccount> ryan = createBankAccount();
+        List<BancAccount> patrick = createBankAccount();
         Gson gson = new Gson();
+        Gson gson1 = new Gson();
         String json = gson.toJson(ryan);
+        String json1 = gson1.toJson(patrick);
         System.out.println(json);
+        System.out.println(json1);
 
     }
 
-    private static BancAccount createBankAccount() {
+    //need to return mult. bank accounts, so a list
+    /*private static BancAccount createBankAccount() {    //List<BancAcoount>
 
         BancAccount ryan = new BancAccount();
         ryan.setAccountName("Ryan's Account");
@@ -25,7 +33,24 @@ public class GsonPrinter {
         ryan.setAccountNumber(2);
 
         return ryan;
+    }*/
 
+    private static List<BancAccount> createBankAccount() {
+
+        BancAccount patrick = new BancAccount();
+        patrick.setAccountName("Patrick's Account");
+        patrick.setBalance(1_000);
+        patrick.setAccountNumber(1);
+
+        BancAccount ryan = new BancAccount();
+        ryan.setAccountName("Ryan's Account");
+        ryan.setBalance(10_000);
+        ryan.setAccountNumber(2);
+
+        return Collections.singletonList(patrick);
+        //return Collections.singletonList(ryan);
+        //when the above is not comment out, getting error;
+        //java:unreachable statement
     }
 
 }
