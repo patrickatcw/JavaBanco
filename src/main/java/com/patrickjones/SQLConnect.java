939e588a30fe317155cf2f3090baf5e479642d07
patrickatcw <pj8392@yahoo.com>
2018-04-12 20:51:53 -0400

    further work on returning account name and fix catch / throw exceptions

--- src/main/java/com/patrickjones/SQLConnect.java
+++ src/main/java/com/patrickjones/SQLConnect.java
@@ -16,7 +16,8 @@ public class SQLConnect {
             Class.forName("com.mysql.jdbc.Driver");
             // Setup the connection with the DB
 
-            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/javabanco?autoReconnect=true&useSSL=false", "gweedo", "gweedopw");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/javabanco?autoReconnect=" +
+                    "true&useSSL=false", "gweedo", "gweedopw");
 
             // Statements allow to issue SQL queries to the database
             statement = connect.createStatement();
@@ -25,30 +26,6 @@ public class SQLConnect {
                     .executeQuery("select * from javabanco.bank_account");
             writeResultSet(resultSet);
 
-            // PreparedStatements can use variables and are more efficient
-            /*preparedStatement = connect
-                    .prepareStatement("insert into  feedback.comments values (default, ?, ?, ?, ? , ?, ?)");
-            // "myuser, webpage, datum, summary, COMMENTS from feedback.comments");
-            // Parameters start with 1
-            preparedStatement.setString(1, "Test");
-            preparedStatement.setString(2, "TestEmail");
-            preparedStatement.setString(3, "TestWebpage");
-            preparedStatement.setDate(4, new java.sql.Date(2009, 12, 11));
-            preparedStatement.setString(5, "TestSummary");
-            preparedStatement.setString(6, "TestComment");
-            preparedStatement.executeUpdate();
-
-            preparedStatement = connect
-                    .prepareStatement("SELECT myuser, webpage, datum, summary, COMMENTS from feedback.comments");
-            resultSet = preparedStatement.executeQuery();
-            writeResultSet(resultSet);
-
-            // Remove again the insert comment
-            preparedStatement = connect
-                    .prepareStatement("delete from feedback.comments where myuser= ? ; ");
-            preparedStatement.setString(1, "Test");
-            preparedStatement.executeUpdate();
-*/
             resultSet = statement
                     .executeQuery("select * from javabanco.bank_account");
             writeMetaData(resultSet);
@@ -70,6 +47,7 @@ public class SQLConnect {
         System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
         for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
             System.out.println("Column " + i + " " + resultSet.getMetaData().getColumnName(i));
+
         }
     }
 
@@ -78,17 +56,10 @@ public class SQLConnect {
         while (resultSet.next()) {
             // It is possible to get the columns via name
             // also possible to get the columns via the column number
-            // which starts at 1
-            // e.g. resultSet.getSTring(2);
-            /*String user = resultSet.getString("myuser");
-            String website = resultSet.getString("webpage");
-            String summary = resultSet.getString("summary");
-            Date date = resultSet.getDate("datum");*/
+            // which starts at 1, e.g. resultSet.getSTring(2);
+
             String comment = resultSet.getString("account_name");
-           /* System.out.println("User: " + user);
-            System.out.println("Website: " + website);
-            System.out.println("summary: " + summary);
-            System.out.println("Date: " + date);*/
+
             System.out.println("Account Name: " + account_name);
         }
     }
