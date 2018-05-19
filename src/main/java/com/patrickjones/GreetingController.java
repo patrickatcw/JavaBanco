18b3e32bf5ab8600f06cd1887860b6773534bf2f
patrickatcw <pj8392@yahoo.com>
2018-05-18 22:29:44 -0400

    working on take a form submission, need more work but making good progress

--- src/main/java/com/patrickjones/GreetingController.java
+++ src/main/java/com/patrickjones/GreetingController.java
@@ -3,18 +3,24 @@ package com.patrickjones;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class SpringController {
+public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name",
             required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
-
-
         return "greeting";
+
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
     }
 
 }
