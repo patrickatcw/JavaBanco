dfbbbd27af8fa8cda91e3e18146e314e44c0c959
patrickatcw <pj8392@yahoo.com>
2018-05-28 09:23:08 -0400

    take form submission complete

--- src/main/java/com/patrickjones/Application.java
+++ src/main/java/com/patrickjones/Application.java
@@ -1,14 +1,42 @@
 package com.patrickjones;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
-    public static void main(String[] args) {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
-
     }
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        log.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "account_number VARCHAR(255), amount VARCHAR(255))");
+
+        // Uses JdbcTemplate's batchUpdate operation to bulk load data
+        jdbcTemplate.batchUpdate("INSERT INTO customers(account_number, amount) VALUES (?,?)");
+
+        log.info("Querying for customer records where account_number = '33':");
+
+        jdbcTemplate.query(
+                "SELECT account_number, amount FROM customers WHERE account_number = ?", new Object[]{"33"},
+                (rs, rowNum) -> new Customer(rs.getLong("account_number"), rs.getDouble("amount"))
+        ).forEach(customer -> log.info(customer.toString()));
+    }
 }
---
+++ src/main/java/com/patrickjones/Customer.java
@@ -0,0 +1,38 @@
+package com.patrickjones;
+
+//https://spring.io/guides/gs/relational-data-access/
+
+public class Customer {
+
+    private long accountNumber;
+    private double amount;
+
+    public Customer(long accountNumber, double amount) {
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[accountNumber='%s', amount='%s']",
+                accountNumber, amount);
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+
+        this.accountNumber = accountNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
